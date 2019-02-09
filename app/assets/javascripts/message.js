2757946af7106beaf479e2525ae7b1aab07cc657
tsurutadesu <k.tsuruta.1108@gmail.com>
2019-02-09 17:30:53 +0900

    Asynchronous communication

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,56 @@
+$(function(){
+
+  function buildHTML(message){
+    var body  = (message.body) ? message.body : '';
+    if (message.image) {
+      if (body) {
+        var html = `<div class="message">
+                      <p class="member-name">${message.name}</p>
+                      <p class="message-time">${message.time}</p>
+                      <p class="chat-text">${body}</p>
+                      <p class="chat-text"><img class="image" src="${message.image}"></p>
+                    </div>`
+      } else {
+        var html = `<div class="message">
+                      <p class="member-name">${message.name}</p>
+                      <p class="message-time">${message.time}</p>
+                      <p class="chat-text"><img class="image" src="${message.image}"></p>
+                    </div>`
+      }
+    } else {
+        var html = `<div class="message">
+                      <p class="member-name">${message.name}</p>
+                      <p class="message-time">${message.time}</p>
+                      <p class="chat-text">${body}</p>
+                    </div>`
+    }
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    console.log(this)
+    var formData = new FormData(this);
+    var url = window.location.pathname;
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+    })
+    .done(function(data){
+      console.log('succes!');
+      console.log(data);
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val('');
+      $('.upload-icon').val('');
+      $('.form__submit').prop('disabled', false);
+    })
+    .fail(function(data){
+      console.log('error');
+    });
+  });
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,6 @@
+json.name @message.user.name
+json.body @message.body
+json.image @message.image.url
+json.user_id @message.user_id
+json.group_id @message.group_id
+json.time @message.created_at
