4cfaa27bc9f99b693cd35d4dff508dcdb9dfe3fb
tsurutadesu <k.tsuruta.1108@gmail.com>
2019-02-11 14:51:43 +0900

    change method name

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,7 +2,7 @@ $(function() {
 
   var search_list = $('#user-search-result');
 
-  function appendUser(user){
+  function appendSearchUserResult(user){
     var html =`<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
                 <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
@@ -10,13 +10,12 @@ $(function() {
     search_list.append(html)
   }
 
-  function buildHTML(user){
+  function buildAddUserHTML(user){
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${user.id}'>
                   <input name='group[user_ids][]' type='hidden' value='${user.id}'>
                   <p class='chat-group-user__name'>${user.name}</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' data-user-id=${user.id}>削除</a>
               </div>`
-
     return html
   }
 
@@ -32,7 +31,7 @@ $(function() {
       $('#user-search-result').empty();
       if(users.length !== 0) {
         users.forEach(function(user){
-          appendUser(user);
+          appendSearchUserResult(user);
         });
       }
       $('.chat-group-form__field').ready(function(){
@@ -41,7 +40,7 @@ $(function() {
           var id = $(this).attr('data-user-id');
           users.forEach(function(user){
             if (user.id == id){
-              var html = buildHTML(user);
+              var html = buildAddUserHTML(user);
               $('#chat-group-users').append(html);
               $('#user-search-result').empty();
             }
