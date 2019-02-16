e90e92cdd97834392795378830563d62b34c1e4d
tsurutadesu <k.tsuruta.1108@gmail.com>
2019-02-16 20:58:17 +0900

    add text

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,12 @@
+set :linked_files, %w{ config/secrets.yml }
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 # config valid only for current version of Capistrano
 lock '3.11.0'
 
@@ -21,4 +30,16 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
