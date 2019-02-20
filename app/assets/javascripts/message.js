5012d42470e1079fbfa850aae1e655a4f96594c8
tsurutadesu <k.tsuruta.1108@gmail.com>
2019-02-20 19:41:02 +0900

    change views & JS files

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,12 +1,16 @@
 $(function(){
 
   function buildHTML(message){
-    var image = (message.image) ? `<p class="chat-text"><img class="image" src="${message.image}"></p>` : '';
+    var image = (message.image) ? `<img class="lower-message__image" src="${message.image}">` : '';
     var html = `<div class="message" data-id="${message.id}">
-                  <p class="member-name">${message.name}</p>
-                  <p class="message-time">${message.time}</p>
-                  <p class="chat-text">${message.body}</p>
-                  ${image}
+                  <div class="upper-message">
+                    <div class="upper-message__member-name">${message.name}</div>
+                    <div class="upper-message__time">${message.time}</div>
+                  </div>
+                  <div class="lower-meesage">
+                    <p class="lower-message__chat-text">${message.body}</p>
+                    ${image}
+                  </div>
                 </div>`
 
     return html;
@@ -27,6 +31,7 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'swing')
       $('#new_message')[0].reset();
       $('.form__submit').prop('disabled', false);
     })
