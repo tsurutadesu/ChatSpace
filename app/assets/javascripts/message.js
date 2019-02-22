4ea2d90d8d9937b62c9d9006f7c804edf9cdcad2
tsurutadesu <k.tsuruta.1108@gmail.com>
2019-02-22 15:22:54 +0900

    add text

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -31,7 +31,7 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'swing')
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'swing');
       $('#new_message')[0].reset();
       $('.form__submit').prop('disabled', false);
     })
